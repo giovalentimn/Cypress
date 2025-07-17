7f7ec5bb94790b6c211e9a76ed7111bd1dc4cd08
Giovanna Silva <92923479+giovalentimn@users.noreply.github.com>
2025-07-17 15:21:04 -0400

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: '7b822fa0-9cb8-4033-9995-108b238f8dc2',
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {}
